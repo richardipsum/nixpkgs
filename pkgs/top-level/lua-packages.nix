15eb86012fe2a9ccbb9d9e2e70fb258ee3a8236a
Richard Ipsum <richardipsum@fastmail.co.uk>
2017-05-28 16:53:17 +0100

    stuff

--- pkgs/top-level/lua-packages.nix
+++ pkgs/top-level/lua-packages.nix
@@ -215,6 +215,32 @@ let
     '';
   };
 
+  luascrypt = buildLuaPackage rec {
+    name = "luxio-${version}";
+    version = "12";
+    #src = fetchurl {
+    #  url = "https://git.gitano.org.uk/luxio.git/snapshot/luxio-luxio-12.tar.bz2";
+    #  sha256 = "18lykif8xi8q4n04d9dnds9ih8149hqnjxpn7hzm4hmz3l2pzyjj";
+    #};
+    src = /home/richardipsum/projects/contrib/lua-scrypt;
+    buildInputs = [ which pkgconfig ];
+    meta = {
+      platforms = stdenv.lib.platforms.unix;
+      license = stdenv.lib.licenses.mit;
+    };
+    preBuild = ''
+      makeFlagsArray=(
+        INST_LIBDIR="$out/lib/lua/${lua.luaversion}"
+        INST_LUADIR="$out/share/lua/${lua.luaversion}"
+        #LUA_BINDIR="$out/bin/lua/${lua.luaversion}"
+        LUA_BINDIR="$out/bin"
+        #LUA_BINDIR="${lua}/bin"
+        #LUA_INCDIR="-I${lua}/include"
+        #LUA_LIBDIR="-L${lua}/lib"
+        );
+    '';
+  };
+
   luasec = buildLuaPackage rec {
     name = "sec-0.6";
     src = fetchFromGitHub {
